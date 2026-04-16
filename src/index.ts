868c6e95048c2549bfa7221af348999a559c373f
ghaininath-z2211 <ghaininath.suryavanshi@zeksta.com>
2026-04-16 13:44:55 +0530

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
